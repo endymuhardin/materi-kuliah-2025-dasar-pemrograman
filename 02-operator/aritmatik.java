480f15c1f61b3b02c07e35c0cb7222427eb74a81
Endy Muhardin <endy.muhardin@gmail.com>
2025-10-23 11:43:11 +0700

    contoh operator aritmatika

---
+++ 02-operator/aritmatik.java
@@ -0,0 +1,19 @@
+void main(){
+    // operator aritmatika : untuk operasi matematika
+    // + : pertambahan
+    // * : perkalian
+    // / : pembagian
+    // - : pengurangan
+    // % : sisa bagi / modulus
+    // << dan >> : shift operator
+
+    int a = 15;
+    int b = 7;
+    System.out.println("a + b = "+ (a + b));
+    System.out.println("a - b = "+ (a - b));
+    System.out.println("a * b = "+ (a * b));
+    System.out.println("a / b = "+ (a / b)); // ini harus diperbaiki supaya hasilnya benar
+    System.out.println("a % b = "+ (a % b));
+
+    // perbaikan : lakukan type casting / type conversion pada tempat yang sesuai
+}
